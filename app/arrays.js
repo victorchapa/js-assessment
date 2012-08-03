3ea4e92722c7b7e98cbb6d94b3af24f55554bf1d
Victor Chapa Calvillo <vchapa@Victors-MacBook.local>
2012-08-03 16:42:35 -0500

    Concluded all testinf of arrays.

--- app/arrays.js
+++ app/arrays.js
@@ -23,35 +23,65 @@ define(function() {
     },
 
     append : function(arr, item) {
-
+      arr.push(item);
+      return arr; 
     },
 
     truncate : function(arr) {
-
+      arr.pop();
+      return arr;
     },
 
     concat : function(arr1, arr2) {
-
+      var arr3 = arr1.concat(arr2);
+      return arr3;
     },
 
     insert : function(arr, item, index) {
-
+      arr.splice(2,0,item);
+      return arr;
     },
 
     count : function(arr, item) {
-
+      var cont = 0;
+      for (var x=0; x<=arr.length-1; x++){
+        if (arr[x] === item){
+          cont = cont + 1;
+        }
+      }
+      return cont;
     },
 
     duplicates : function(arr) {
-
+      var arr = arr.sort(),
+          rep = [];
+      for(var x=0; x<=arr.length-1; x++){
+          var findstart = arr.indexOf(x),
+              findend = arr.lastIndexOf(x);
+          if(findstart != findend){
+              rep.push(x);
+          }
+      }
+      return rep;
     },
 
     square : function(arr) {
-
+      for(var x=0; x<=arr.length-1; x++){
+        arr[x] = arr[x] * arr[x];
+      }
+      return arr;
     },
 
     findAllOccurrences : function(arr, target) {
-
+      var rep = [];
+      for(var x=0; x<=arr.length-1; x++){
+        var find = arr.indexOf(target);
+        if(find>=0){
+          arr.splice(find,1,"*");
+          rep.push(find);
+        }
+      }
+      return rep;
     }
   };
 });
