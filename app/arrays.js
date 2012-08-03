c512579e15daa5a0b826ede06e8b7de3ef931fd8
Victor Chapa Calvillo <vchapa@Victors-MacBook.local>
2012-08-03 14:26:24 -0500

    Concluded 3 array examples.

--- app/arrays.js
+++ app/arrays.js
@@ -3,15 +3,23 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     indexOf : function(arr, item) {
-
+      var a = [1,2,3,4];
+      return a[1];
     },
 
     sum : function(arr) {
-
+      var sum = 0;
+      for(var x=0; x <= arr.length-1; x++){
+        sum = sum + arr[x];
+      }
+      return sum;
     },
 
     remove : function(arr, item) {
-
+      var array = arr.sort(),
+          pot = array.indexOf(2);
+      array.splice(pot,2);
+      return array;
     },
 
     append : function(arr, item) {
