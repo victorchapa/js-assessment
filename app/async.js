7bdc6f2c6c04cc45ffa193a1a97564a4ff70f022
Victor Chapa Calvillo <vchapa@Victors-MacBook.local>
2012-08-03 19:23:28 -0500

    Finished Logical Operators and Ajax petition.

--- app/async.js
+++ app/async.js
@@ -3,11 +3,23 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     async : function() {
-
     },
 
     manipulateRemoteData : function(url) {
-
+      return {
+        then: function(cb){
+          $.ajax({
+            url: url
+          }).done(function(data){
+            var party = [];
+            for(var x=0; x<=data.people.length-1; x++){
+              var person = data.people[x].name;
+              party.push(person);
+            }
+            return cb(party.sort());
+          });
+        }
+     }
     }
   };
 });
